0ee7ea18df8e12c8502d19ec8fb741d2fcbbf48b
Jonathan Stuart-Moore <jstuartm@gmail.com>
2011-10-20 12:55:46 -0400

    CLEANUP: updated formatting

--- Cues/readcuedata.js
+++ Cues/readcuedata.js
@@ -4,47 +4,47 @@ nextRow = 0;
 key = [];
 
 function rowFromText(text) {
-	parts = text.split('\t');
-	rows[nextRow] = [];
-	rows[nextRow]['cue'] = parts[0];
-	rows[nextRow]['cue_text'] = parts[1];
-	rows[nextRow]['patcher_file'] = parts[2];
-	rows[nextRow]['sub_patcher'] = parts[3];
+    parts = text.split('\t');
+    rows[nextRow] = [];
+    rows[nextRow]['cue'] = parts[0];
+    rows[nextRow]['cue_text'] = parts[1];
+    rows[nextRow]['patcher_file'] = parts[2];
+    rows[nextRow]['sub_patcher'] = parts[3];
 
-	rows[nextRow]['messages'] = [];
-	messages = parts[4].split(';');
-	for(j=0;j<messages.length;j++) {
-		thism = messages[j].split(':');
-		rows[nextRow]['messages'][j] = [];
-		rows[nextRow]['messages'][j]['to'] = thism[0];
-		rows[nextRow]['messages'][j]['args'] = thism[1];
-	}
-	nextRow++;
+    rows[nextRow]['messages'] = [];
+    messages = parts[4].split(';');
+    for(j=0;j<messages.length;j++) {
+        thism = messages[j].split(':');
+        rows[nextRow]['messages'][j] = [];
+        rows[nextRow]['messages'][j]['to'] = thism[0];
+        rows[nextRow]['messages'][j]['args'] = thism[1];
+    }
+    nextRow++;
 }
 
 function keyRowFromText(text) {
-	parts = text.split('\t');
-	key[parts[0]] = parts[1];
+    parts = text.split('\t');
+    key[parts[0]] = parts[1];
 }
 
 function readCueData(p) {
-	path = p;
-	lines = new Array();
-	var f = new File(path,"read");
-	f.open();
-	// get rid of first line (header)
-	if (f.isopen) {
-		f.readline();
-	}
-	// read the data
-	if (f.isopen) {
-		while(f.position<f.eof) {
-			rowFromText(f.readline());
-		}
-		f.close();
-	} else {
-		post("Error Reading Cue Data File\n");
-	}
+    path = p;
+    lines = new Array();
+    var f = new File(path,"read");
+    f.open();
+    // get rid of first line (header)
+    if (f.isopen) {
+        f.readline();
+    }
+    // read the data
+    if (f.isopen) {
+        while(f.position<f.eof) {
+            rowFromText(f.readline());
+        }
+        f.close();
+    } else {
+        post("Error Reading Cue Data File\n");
+    }
 }
 
 function query(q) {
